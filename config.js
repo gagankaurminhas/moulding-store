6a670011c89c2c1fba3e4dcf3a5d62edc401407a
gagankaurminhas <g.gaganminhas@gmail.com>
2026-09-21 21:20:58 -0600

    Refactor APP_CONFIG structure in config.js

--- config.js
+++ config.js
@@ -1,15 +1,21 @@
 const APP_CONFIG = {
   clientId: "ed8479d2-5a79-4030-9b29-007ae8b98dcd",
   tenantId: "9b01cfb5-bb13-476b-be96-44f4139eaa56",
-  redirectUri: "https://gagankaurminhas.github.io/moulding-store/",
 
-  // Exact file name in your OneDrive root (My files).
-  workbookFileName: "MouldingStoreDatabase.xlsx",
+  redirectUri:
+    "https://gagankaurminhas.github.io/moulding-store/",
 
-  // Filled in after the one-time owner setup.
+  workbookFileName:
+    "MouldingStoreDatabase.xlsx",
+
   workbookDriveId: "",
   workbookItemId: "",
 
-  scopes: ["User.Read", "Files.ReadWrite"],
-  graphBaseUrl: "https://graph.microsoft.com/v1.0"
+  scopes: [
+    "User.Read",
+    "Files.ReadWrite"
+  ],
+
+  graphBaseUrl:
+    "https://graph.microsoft.com/v1.0"
 };
